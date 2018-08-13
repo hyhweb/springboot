856c5afb62cccf146eb9b2b30716a9550a20d73c
hyhweb <461342854@qq.com>
2018-08-13 14:22:43 +0800

    code

--- src/test/java/com/hotsale/HotsaleApplicationTests.java
+++ src/test/java/com/hotsale/HotsaleApplicationTests.java
@@ -2,11 +2,15 @@ package com.hotsale;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootApplication
+@MapperScan("com.hotsale.*.dao")
 public class HotsaleApplicationTests {
 
 	@Test
